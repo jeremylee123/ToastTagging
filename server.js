39991af28120c552dda38f9d7ae9200b0ee5dd5f
Miles <mblack@platformq.com>
2017-12-03 13:59:58 -0500

    Modified database setup sql script, fixed sql credentials.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   port     : 3306,
   user     : 'root',
+  password : 'root',
   database : 'toasttagging'
 });
 const express = require('express');

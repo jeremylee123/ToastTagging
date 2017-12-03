f39bd9243f961f2aeefd00d0c918ec884aea1ab8
James Curry <james@1x-nat-vl930-172-30-122-136.wireless.umass.edu>
2017-12-03 14:49:23 -0500

    added comments and endpoint with argument

--- server.js
+++ server.js
@@ -9,10 +9,19 @@ var connection = mysql.createConnection({
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+// Use static resources in public folder (HTML, CSS, JS)
+app.use(express.static('public'));
 
+// Provides a list of systems that the user has access to
 app.get('/api/listsystems', function (req, res) {
 	connection.query("SELECT * FROM system", function(error, results, fields) {
+		res.send(results);
+	});
+});
+
+// Provides information about a system based off of the given serialNumber
+app.get('/api/systems', function (req, res) {
+	connection.query("SELECT * FROM system WHERE serialNumber = "+ req.query.serialNumber, function(error, results, fields) {
 		res.send(results);
 	});
 });

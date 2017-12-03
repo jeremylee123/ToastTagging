0ad17fd6f30bdd81393ecf7571f8c5fc257f2bc6
Miles <mblack@platformq.com>
2017-12-03 15:59:10 -0500

    Added functioning to systemlists with 3 different parameter types. Follow conventioning for comments on http requests.

--- server.js
+++ server.js
@@ -12,9 +12,22 @@ const app = express();
 // Use static resources in public folder (HTML, CSS, JS)
 app.use(express.static('public'));
 
-// Provides a list of systems that the user has access to
-app.get('/api/listsystems', function (req, res) {
-	connection.query("SELECT * FROM system", function(error, results, fields) {
+/**
+ * Directory: localhost:3000/api/systemslist
+ * Parameters: systemslist - displays all of the system rows.
+ * 			   systemslist?offset=x - displays the first x rows.
+ * 			   systemslist?offset=x&start=y - displays the first x rows starting at entry y.
+ * The usage of the offset and start parameters can be useful if we want to have
+ * pages that display certain amounts of rows per page. eg. display 10 systems
+ * per page and easily navigate through different pages to calculate the offsets and starting entries.
+ */
+app.get('/api/systemslist', function (req, res) {
+	var queryText = "SELECT * FROM system";
+	if (req.query.offset != null && req.query.start == null)
+		queryText += " LIMIT " + req.query.offset;
+	else if (req.query.offset != null && req.query.start != null)
+		queryText += " LIMIT " + (req.query.start - 1) + ", " + req.query.offset;
+	connection.query(queryText, function(error, results, fields) {
 		res.send(results);
 	});
 });

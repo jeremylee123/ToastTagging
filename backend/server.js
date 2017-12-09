2510bcf306cdb8d9ba84c9b3c63b398fc741e4c4
Miles <mblack@platformq.com>
2017-12-09 14:43:00 -0500

    Added error handling and combined systemslist and listsystems into listsystems endpoints. Documentation added.

--- backend/server.js
+++ backend/server.js
@@ -79,34 +79,29 @@ app.post('/api/login', function (req, res) {
   });
 });
 
-app.get('/api/listsystems', function (req, res) {
-  connection.query("SELECT * FROM system", function(error, results, fields) {
-    if(error) {
-      console.log("Error connecting to db");
-    }
-    res.send(results);
-  });
-});
-
 /**
  * Type: GET
- * Directory: localhost:3000/api/systemslist
- * Parameters: systemslist - displays all of the system rows.
- *         systemslist?offset=x - displays the first x rows.
- *         systemslist?offset=x&start=y - displays the first x rows starting at entry y.
+ * Directory: localhost:3000/api/listsystems
+ * Parameters: listsystems - displays all of the system rows.
+ *         listsystems?offset=x - displays the first x rows.
+ *         listsystems?offset=x&start=y - displays the first x rows starting at entry y.
  * The usage of the offset and start parameters can be useful if we want to have
  * pages that display certain amounts of rows per page. eg. display 10 systems
  * per page and easily navigate through different pages to calculate the offsets and starting entries.
  */
-app.get('/api/systemslist', function (req, res) {
-  var queryText = "SELECT * FROM system";
-  if (req.query.offset != null && req.query.start == null)
-    queryText += " LIMIT " + req.query.offset;
-  else if (req.query.offset != null && req.query.start != null)
-    queryText += " LIMIT " + (req.query.start - 1) + ", " + req.query.offset;
-  connection.query(queryText, function(error, results, fields) {
-    res.send(results);
-  });
+app.get('/api/listsystems', function (req, res) {
+	var queryText = "SELECT * FROM system";
+	if (req.query.offset != null && req.query.start == null)
+		queryText += " LIMIT " + req.query.offset;
+	else if (req.query.offset != null && req.query.start != null)
+		queryText += " LIMIT " + (req.query.start - 1) + ", " + req.query.offset;
+	connection.query(queryText, function(error, results, fields) {
+	  if(error) {
+		  res.send(error);
+	  } else {
+		  res.send(results);
+	  }
+	});
 });
 
 /**

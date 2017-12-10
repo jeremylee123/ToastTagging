9307d686c90f9ce90cfe781648d3454206e6fa04
James Curry <james@1x-nat-vl930-172-30-119-33.wireless.umass.edu>
2017-12-10 14:51:29 -0500

    added system info display

--- frontend/js/SystemInfo/components/SystemInfo.js
+++ frontend/js/SystemInfo/components/SystemInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Label, Menu, Table, Input } from 'semantic-ui-react'
+import { Icon, Label, Menu, Table, Input, Header } from 'semantic-ui-react'
 
 import SystemTag from './SystemTag';
 
@@ -12,22 +12,28 @@ class SystemInfo extends React.Component {
   // }
   render() {
     const tags = this.props.info.tags;
+    const systemName = this.props.info.systemInfo.systemName;
+    const companyName = this.props.info.systemInfo.companyName;
     console.log(this.props.info)
     if(tags) {
       return (
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell collapsing>{"Remove"}</Table.HeaderCell>
-              <Table.HeaderCell>{"Tag Name"}</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {tags.map(tag =>
-      				<SystemTag key={tag.id} tag={tag} />
-      			)}
-          </Table.Body>
-        </Table>
+        <div>
+          <Header as="h1">System: {systemName}</Header>
+          <Header sub>Company: {companyName}</Header>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell collapsing>{"Remove"}</Table.HeaderCell>
+                <Table.HeaderCell>{"Tag Name"}</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {tags.map(tag =>
+        				<SystemTag key={tag.id} tag={tag} />
+        			)}
+            </Table.Body>
+          </Table>
+        </div>
       )
     } else {
       return (<div></div>)
